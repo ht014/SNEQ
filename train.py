937426ba20f4a9588fe5b5e7193cecf49bb77ee9
HT <hetaoconquer@gmail.com>
2021-05-29 17:58:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import scipy.sparse as sp
-from model import Graph2Gauss
+from model import SNEQ
 from utils import load_dataset, score_link_prediction, score_node_classification
 import os
 import sys
@@ -19,10 +19,10 @@ name = sys.argv[1]
  
 
 if input_X =='attribute':
-    g2g = Graph2Gauss(input=input_X,A=A, z= zz,data_ = name,X=X,L=128,batch = int(sys.argv[6]),
+    g2g = SNEQ(input=input_X,A=A, z= zz,data_ = name,X=X,L=128,batch = int(sys.argv[6]),
                       p_semi = p_lab, K=int(sys.argv[4]), p_val=0.05, p_test=0.1, p_nodes=0.0)
 if input_X =='adjacent':
-    g2g = Graph2Gauss(input=input_X,A=A, z= zz, data_=name,batch = int(sys.argv[6]),
+    g2g = SNEQ(input=input_X,A=A, z= zz, data_=name,batch = int(sys.argv[6]),
                       X=A + F * sp.eye(A.shape[0]), p_semi = p_lab,L=128,  K=int(sys.argv[4]), p_val=0.05,
                       p_test=0.1, p_nodes=0.0)
 if isinstance(z,np.ndarray) == False:
